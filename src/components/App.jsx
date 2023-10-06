b737854d67fdf5f2993527b4f7a5803490a4b7c6
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-10-06 23:08:47 +0200

    Add app draft

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import { styled } from "styled-components";
+import { Home } from "Pages/Home/Home";
+import { Movies } from "Pages/Movies/Movies";
+import { MovieInfo } from "Pages/MoviesInfo/MoviesInfo";
+import { Cast } from "./Cast/Cast";
+import { Reviews } from "./Reviews/Reviews";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <>
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies/:movieId" element={<MovieInfo />} />
+        <Route path="/movies/:movieId/cast" element={<Cast />} />
+        <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+      </Routes>
+    </>
+  )
+}
