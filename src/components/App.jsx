90afd9bf8e5cdd886b5146d61170febcbf7ae210
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-10-07 14:00:10 +0200

    Add lazy and suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,35 @@
-import { Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { Home } from "Pages/Home/Home";
-import { Movies } from "Pages/Movies/Movies";
-import { MovieInfo } from "Pages/MoviesInfo/MoviesInfo";
-import { Cast } from "./Cast/Cast";
-import { Reviews } from "./Reviews/Reviews";
+// import { Movies } from "Pages/Movies/Movies";
+// import { MovieInfo } from "Pages/MoviesInfo/MoviesInfo";
+// import { Cast } from "./Cast/Cast";
+// import { Reviews } from "./Reviews/Reviews";
 
 import { Nav, Link } from "./App.styled";
 
+const Movies = lazy(() => import('Pages/Movies/Movies'));
+const MovieInfo = lazy(() => import('Pages/MoviesInfo/MoviesInfo'));
+const Cast = lazy(() => import('Components/Cast/Cast'));
+const Reviews = lazy(() => import('Components/Reviews/Reviews'));
+
 export const App = () => {
   return (
-  <>
+    <>
         <Nav>
           <Link to="/">Home</Link>
           <Link to="/movies">Movies</Link>
         </Nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieInfo />}>
-            <Route path="cast" element={<Cast />} />
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-      </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/movies" element={<Movies />} />
+            <Route path="/movies/:movieId" element={<MovieInfo />}>
+              <Route path="cast" element={<Cast />} />
+              <Route path="reviews" element={<Reviews />} />
+            </Route>
+        </Routes>
+      </Suspense>
     </>
-  );
+  )
 }
