a74a290a3b8ffdd49a245dbbdc1e5a2202c59bd5
Mijaae <117861571+Mijaae@users.noreply.github.com>
2023-10-07 15:39:18 +0200

    Small update to styles

--- src/Pages/MoviesInfo/MoviesInfo.styled.js
+++ src/Pages/MoviesInfo/MoviesInfo.styled.js
@@ -5,17 +5,14 @@ export const Button = styled.button`
   margin: 15px;
   padding: 3px 20px;
   border: none;
-  &:hover {
-    cursor: pointer;
-    transition-duration: 0.4s;
-  }
-  
+  border-radius: 5px;
+  cursor: pointer;
+  transition: 0.25s;
 `
 
 export const Wrapper = styled.div`
 
   display: flex;
-
 `
 
 export const MovieImage = styled.div`
